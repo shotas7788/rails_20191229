7674ecb62a38b96d943c75fe8693e24803953b6e
vagrant <vagrant@localhost.localdomain>
2020-01-13 12:47:12 +0000

    終了

--- spec/system/food_enquetes_spec.rb
+++ spec/system/food_enquetes_spec.rb
@@ -46,4 +46,30 @@ RSpec.describe "FoodEnquetes", type: :system do
       end
     end
   end
+
+  describe '異常' do
+    context "必須項目が未入力" do
+      it "エラーメッセージが表示され、回答できないこと" do
+        # [Point.6-3-1]アンケートページにアクセスします。
+        visit "/food_enquetes/new"
+
+        # 【補足】見た目をわかりやすくするために動作を2秒止めています。
+        # 通常は自動テストが遅くなりますので不要な処理です。
+        sleep 2
+        # [Point.6-3-2]「登録する」ボタンをクリックします。
+        click_button '登録する'
+        # [Point.6-3-3]回答完了時のメッセージが含まれないことを検証します。
+        expect(page).not_to have_content 'ご回答ありがとうございました'
+        # [Point.6-3-4]必須入力のメッセージが含まれることを検証します。
+        expect(page).to have_content 'お名前を入力してください'
+        expect(page).to have_content 'メールアドレスを入力してください'
+        expect(page).to have_content '年齢を入力してください'
+        expect(page).to have_content '満足度を入力してください'
+
+        # 【補足】見た目をわかりやすくするために動作を5秒止めています。
+        # 通常は自動テストが遅くなりますので不要な処理です。
+        sleep 5
+      end
+    end
+  end
 end
